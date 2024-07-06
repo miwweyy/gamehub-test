9cf36300dd4174d066332f4afb1df55f38ba8300
kanyanatsk2002 <kanyanat.sk.2002@gmail.com>
2024-07-06 21:24:03 +0700

    Clerk redirecting to localhost after authentication

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks(.*)"]
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
